4c562287f023877d253d16373eb465f13158e41c
fudro <fudro1@gmail.com>
2019-04-06 14:03:41 -0700

    adding new scripts

---
+++ HelloAR/Assets/LookController.cs
@@ -0,0 +1,78 @@
+﻿/**************************************
+ This script allows the attached AR gameobject to react to screen touches by turning or moving towards the touch point.
+ ************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LookController : MonoBehaviour
+{
+    enum TurnMode {Quick, Smooth, Move};
+    int mode = (int)TurnMode.Move;
+    Vector3 touchPoint;     
+    Vector3 lookDirection;      
+    Quaternion rotationAngle;   //Difference between the gamobject's current facing direction and the direction to look
+    float turnSpeed = 5.0f;
+    float moveSpeed = 10.0f;
+    bool turning = false;       //Flag to indicate if the gameobject has completed its turn or is still turning
+
+    void Start()
+    {
+        //Not Used.
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))    //If the left mousebutton is clicked (or the screen is touched on mobile)
+        {
+            SetTouchPoint();        
+        }
+        if (turning)    //Continue turning while this flag is true
+        {
+        switch(mode)    //Select the type of turn reaction
+            {
+                case (int)TurnMode.Smooth:
+                    smoothLook();
+                    break;
+                case (int)TurnMode.Move:
+                    smoothLook();
+                    moveVirtualTarget();
+                    break;
+                default:
+                    this.transform.LookAt(touchPoint);
+                    break;
+            }
+        }
+    }
+
+    void SetTouchPoint()    //Find the in-world point of the touch.
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            touchPoint = hit.point;
+            turning = true;
+        }
+    }
+
+    void smoothLook()
+    {
+        lookDirection = new Vector3(touchPoint.x - transform.position.x, transform.position.y, touchPoint.z - transform.position.z);    //The X and Z axes form the ground plane where the gameobject rests. Y-axis is the upward direction (axis of rotation).
+        rotationAngle = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, turnSpeed * Time.deltaTime);   //Use a Slerp to smoothly turn through the rotation angle to end facing the direction of the touch.
+    }
+
+    void moveVirtualTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, touchPoint, moveSpeed * Time.deltaTime);   //Smoothly move the gamobject towards the touch point.
+        if (transform.position == touchPoint)
+        {
+            turning = false;
+        }
+        
+    }
+}
---
+++ HelloAR/Assets/TouchManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	Ray GenerateMouseRay() 
+	{
+		//Get two points through orthographic projection of the screen touch position to the near and far clipping planes of the main camera.
+		Vector3 mouseTouchFar = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);								
+		Vector3 mouseTouchNear = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+
+		//Convert the points to world coordinates
+		Vector3 mouseWorldFar = Camera.main.ScreenToWorldPoint(mouseTouchFar);
+		Vector3 mouseWorldNear = Camera.main.ScreenToWorldPoint (mouseTouchNear);
+
+		Ray mouseRay = new Ray (mouseWorldNear, mouseWorldFar - mouseWorldNear);
+		return mouseRay;
+	}
+
+    // Update is called once per frame
+    void Update()	
+    {
+		if (Input.GetMouseButton(0))
+		{
+			Ray mouseRay = GenerateMouseRay ();
+			RaycastHit hit;
+
+			if (Physics.Raycast (mouseRay.origin, mouseRay.direction, out hit))
+			{
+                //Add code for touches here
+			}
+		}
+    }
+}
